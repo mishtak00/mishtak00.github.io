0c7a9c9b8a52a1135300c96dc8561646caa5179d
Gebri Mishtaku <gebrimishtaku@hotmail.com>
2022-08-20 23:22:46 -0700

    Updates aug 2022

---
+++ precache-manifest.d818d8ce590d2e2448cb839b173d701d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee8b769cd284dcc7bcea6b1e8f5c077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168c9a9e5005e3c4e88e",
+    "url": "/static/css/main.52ef1d97.chunk.css"
+  },
+  {
+    "revision": "d75c0d0fa3b9030ffac3",
+    "url": "/static/js/2.e7a90cf1.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.e7a90cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168c9a9e5005e3c4e88e",
+    "url": "/static/js/main.c8978164.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "500dcf9f96afe5ad2bdc30354e2fd857",
+    "url": "/static/media/beach.500dcf9f.jpeg"
+  },
+  {
+    "revision": "6b0837939df01eedfc32e3a5decde840",
+    "url": "/static/media/berlin.6b083793.jpeg"
+  },
+  {
+    "revision": "1976fdafed32382950c7398ac6f9ceed",
+    "url": "/static/media/bitmoji.1976fdaf.jpeg"
+  },
+  {
+    "revision": "eb9e0c55cdac4c076b61f99a7621aea1",
+    "url": "/static/media/centerfinder.eb9e0c55.png"
+  },
+  {
+    "revision": "b0dc2f125af7034bc96cf06f61247bdb",
+    "url": "/static/media/contactchain.b0dc2f12.png"
+  },
+  {
+    "revision": "6cfec198d99996396b5e078649315cd8",
+    "url": "/static/media/correlator.6cfec198.png"
+  },
+  {
+    "revision": "dbcf348e289bc8bb6ec7b409e610b47e",
+    "url": "/static/media/dimensions.dbcf348e.png"
+  },
+  {
+    "revision": "408aee9f912bfe9b78eea0e632c2bc1f",
+    "url": "/static/media/genome1.408aee9f.png"
+  },
+  {
+    "revision": "fb537416feead98afc793e0a4606dc3a",
+    "url": "/static/media/gm-icon.fb537416.png"
+  },
+  {
+    "revision": "93700ac3aea20a16bc6871451411c00f",
+    "url": "/static/media/malloc.93700ac3.png"
+  },
+  {
+    "revision": "55d86607e24424a6a90d2e0065170ecd",
+    "url": "/static/media/map1.55d86607.png"
+  },
+  {
+    "revision": "b7e9a03569642bdaf33b116f1d05e866",
+    "url": "/static/media/metrics.b7e9a035.png"
+  },
+  {
+    "revision": "5b905d9a427d90ee83a757291c570673",
+    "url": "/static/media/professional.5b905d9a.jpeg"
+  },
+  {
+    "revision": "0c6272d3a5dced9937582c802f608d98",
+    "url": "/static/media/profile.0c6272d3.jpeg"
+  },
+  {
+    "revision": "c09d7ce658dffd91ba4255f04ad2c579",
+    "url": "/static/media/qparser.c09d7ce6.png"
+  },
+  {
+    "revision": "fb4d3e602ff50a1579f8782b97101304",
+    "url": "/static/media/qsim0.fb4d3e60.png"
+  },
+  {
+    "revision": "b1eb7ef8061b44f7fd46f4222912b7b2",
+    "url": "/static/media/this.b1eb7ef8.png"
+  },
+  {
+    "revision": "3c6b04694a0e6de05e79fb4f940721fc",
+    "url": "/static/media/wun.3c6b0469.png"
+  }
+]);
